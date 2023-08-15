05eb4dcd8590ef98e00b8c0dab7baa1ab1341513
haohaoguo-666 <yyf1519224631@gmail.com>
2023-08-15 23:21:35 +0800

    练习

--- 算法作业/阅读理解/HJ108 求最小公倍数.cpp
+++ 算法作业/阅读理解/HJ108 求最小公倍数.cpp
@@ -1,1 +1,18 @@
 //https://www.nowcoder.com/practice/22948c2cad484e0291350abad86136c3?tpId=37&&tqId=21331&rp=1&ru=/activity/oj&qru=/ta/huawei/question-ranking
+#include<iostream>
+using namespace std;
+int gcd(int a, int b) {
+    int r;
+    while (r = a % b) {
+        a = b;
+        b = r;
+    }
+    return b;
+}
+int main() {
+    int a, b;
+    while (cin >> a >> b) {
+        cout << a* b / gcd(a, b) << endl;
+    }
+    return 0;
+}
